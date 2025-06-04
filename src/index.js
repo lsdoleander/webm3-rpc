812ca9dbed2638b51ed0e61b40097ed1c99ac793
LSD.oleander <lsdoleander@gmail.com>
2025-06-04 04:27:05 -0400

    fixing exports

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 
-import mappings from './mappings.js'
-import prices from './prices.js'
-import balance from './localized.js'
-import wei from './wei.js'
+export {default as mappings} from './mappings.js'
+export {default as prices} from './prices.js'
+export {default as balance} from './localized.js'
+export {default as wei} from './wei.js'
 
-export mappings
-export prices
-export balance
-export wei
-
-export default {
-	mappings,
-	prices,
-	balance,
-	wei
-}
